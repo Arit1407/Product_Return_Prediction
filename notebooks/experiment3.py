b3acc2a00f0c537f71f28a8faddbe445b8e9add7
Arit1407 <105263417+Arit1407@users.noreply.github.com>
2026-01-19 00:46:13 +0530

    pushing Third  experiment

---
+++ notebooks/experiment3.py
@@ -0,0 +1,62 @@
+import os
+import pandas as pd
+import numpy as np
+import mlflow
+import mlflow.xgboost
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.inspection import permutation_importance
+import dagshub
+from xgboost import XGBRegressor
+
+dagshub.init(repo_owner="Arit1407", repo_name="Product_Return_Prediction", mlflow=True)
+mlflow.set_experiment("xgboost")
+
+df = pd.read_csv("C:/Users/HP/Documents/Product_return_prediction_mlops/notebooks/processed-data_for_experiments.csv")
+
+target = "Return Rate"
+X = df.drop(columns=[target])
+y = df[target]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+params = {
+    "n_estimators": 500,
+    "learning_rate": 0.05,
+    "max_depth": 6,
+    "subsample": 0.8,
+    "colsample_bytree": 0.8,
+    "random_state": 42,
+    "n_jobs": -1
+}
+
+with mlflow.start_run(run_name="XGBoost_ReturnRate"):
+
+    xgb = XGBRegressor(**params)
+    xgb.fit(X_train, y_train)
+
+    y_pred = xgb.predict(X_test)
+
+    r2 = r2_score(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+
+    mlflow.log_params(params)
+    mlflow.log_metric("r2", r2)
+    mlflow.log_metric("mae", mae)
+    mlflow.log_metric("mse", mse)
+
+    perm = permutation_importance(xgb, X_test, y_test, n_repeats=5, random_state=42, n_jobs=-1)
+    pi = pd.Series(perm.importances_mean, index=X_test.columns).sort_values(ascending=False)
+    pi.to_csv("xgb_permutation_importance.csv")
+
+    mlflow.log_artifact("xgb_permutation_importance.csv")
+
+    mlflow.xgboost.log_model(xgb, artifact_path="model")
+
+    print("Logged to DAGsHub MLflow")
+    print("R2:", r2)
+    print("MAE:", mae)
+    print("MSE:", mse)
